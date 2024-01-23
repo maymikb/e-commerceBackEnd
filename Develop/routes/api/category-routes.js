ef2dea5ba7c5e4d291e05decfcda745b6c929858
maymikb <123998301+maymikb@users.noreply.github.com>
2024-01-23 12:34:01 -0800

    API,routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,55 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll()
+  .then(results => {
+    res.json(results)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id)
+  .then(results => {
+    res.json(results)
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(results => {
+    res.json(results)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id
+      }
+    },
+    {
+      new: true
+    }
+  ).then(results => {
+    res.json(results)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(results => {
+    res.json(results)
+  })
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -5,33 +5,37 @@ By Maymi Sarr
 ## Table of ContentsS
 - [Purpose](#title)
 - [Description](#description)
-- [Link](#link)
-- [Screenshot](#installation)
-
-
-
-
+- [Link](#Video Walkthrough link)
+- [Installation](#installation)
 
 
 
 
 ## Purpose
-The purpose of this app is. 
+<!-- This app was made to familiarize developers with the back end design for e-commerce sites, because they are prevalent platforms that can generate trillions of dollars yearly. -->
 
 
 ## Description
 With this app, the user is able to  
+<!-- This challenge is to build a back end for e-commerce site[s].
+Internet retail, empowers businesses and consumers to conveniently engage in online buying and selling of electronic products. 2.54 trillion generated. Due to the prevalence of these platforms, developers should understand the fundamental architecture of e-commerce sites.  -->
+
 
 
 
 
 ## Installation
+<div>Express.js</div>
+<div>Sequelize</div>
+<div>MySQL, MySQL2</div>
+<div>dotenv</div>
+<div>Insomnia Core</div>
+
 
 
 ## Link
 
 
-## Screenshot
 
 
 
